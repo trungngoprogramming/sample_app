99cacf2f2cde8efb249a5c3f4648b0f7857b4413
trungngoprogramming <trungngo.programming@gmail.com>
2018-01-10 14:27:37 +0700

    Develop Branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  scope "(:locale)", locale:/en|vi/ do
+    root "static_pages#home"
+
+    get "static_pages/home"
+  end
+
+  scope "(:locale)", locale:/en|vi/ do
+    get "static_pages/help"
+  end
+
+  scope "(:locale)", locale:/en|vi/ do
+    get  "static_pages/about"
+  end
+
+get "static_pages/contact"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
