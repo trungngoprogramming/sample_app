443981217de22d9f445415122493e63420b33203
trungngoprogramming <trungngo.programming@gmail.com>
2018-01-15 11:48:41 +0700

    Filling in layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  scope "(:locale)", locale:/en|vi/ do
+    root "static_pages#home"
+
+    get  "/help",    to: "static_pages#help"
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
