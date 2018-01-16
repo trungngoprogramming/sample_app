aa121625d5343b6502224b84a78b6d98cccadb2c
trungngoprogramming <trungngo.programming@gmail.com>
2018-01-16 15:39:20 +0700

    Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
 
     get  "/help",    to: "static_pages#help"
 
-    get "users/new"
+    get  "/signup",  to: "users#new"
+
+    post "/signup",  to: "users#create"
   end
+    resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
